54681569af702d54ee9bfe922e91453539c3b477
buterbrot359 <batowka359@gmail.com>
2022-05-05 23:05:14 +0600

    feat: add dynamic header and sections to welcome page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,27 @@ import type { NextPage } from 'next';
 import Image from 'next/image';
 import TrelloImage from '../images/Trello.svg';
 import HeroImage from '../images/hero.png';
+import BoardImage from '../images/board.png';
 import styles from '../styles/Welcome.module.scss';
+import { DOMAttributes, UIEventHandler, useEffect, useRef, useState } from 'react';
 
 const Welcome: NextPage = () => {
+  const [header, setHeader] = useState('');
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.pageYOffset > 70) {
+        setHeader('white');
+      } else {
+        setHeader('');
+      }
+    };
+    window.addEventListener('scroll', onScroll, { passive: true });
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
     <>
-      <header className={styles.welcomeHeader}>
+      <header className={`${styles.welcomeHeader} ${styles[header]}`}>
         <Image src={TrelloImage} width={126} height={36}></Image>
         <div className={styles.logBtns}>
           <button className={styles.btns}>Log in</button>
@@ -22,13 +37,36 @@ const Welcome: NextPage = () => {
               Collaborate, manage projects, and reach new productivity peaks. From high rises to the
               home office, the way your team works is unique—accomplish it all with Trello.
             </p>
+            <form action="" className={styles.heroForm}>
+              <input type="email" className={styles.heroForm_input} placeholder="Email"></input>
+              <button type="submit" className={styles.heroForm_btn}>
+                Sign up — it’s free!
+              </button>
+            </form>
           </div>
           <div className={styles.heroRightPart}>
             <Image src={HeroImage}></Image>
           </div>
         </div>
       </section>
-      <section></section>
+      <section id={styles.products}>
+        <div className={styles.container}>
+          <div className={styles.products__topPart}>
+            <h2 className={styles.topPart_title}>
+              It’s more than work. It’s a way of working together.
+            </h2>
+            <p className={styles.topPart_paragraph}>
+              Start with a Trello board, lists, and cards. Customize and expand with more features
+              as your teamwork grows. Manage projects, organize tasks, and build team spirit—all in
+              one place.
+            </p>
+            <a href="" className={styles.topPart_btn}>
+              Start doing →
+            </a>
+            <Image src={BoardImage}></Image>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
