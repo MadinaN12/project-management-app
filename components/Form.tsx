9f99ef9f2ae614376350a1e00e24b8c4c35e0194
buterbrot359 <batowka359@gmail.com>
2022-05-06 15:05:48 +0600

    add: validator to signup form

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,25 +1,83 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
 import styles from '../styles/Form.module.scss';
+import { emailValidator, nameValidator, passwordValidator } from '../utils/validator';
 
 interface IForm {
   isFull: boolean;
   email: string;
 }
+enum RouteEnum {
+  LOGIN = 'login',
+  SIGNUP = 'signup',
+}
 const Form: React.FC<IForm> = ({ isFull, email }) => {
+  const nameInput = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const emailInput = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const password = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const rePassword = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const router = useRouter();
+
+  async function submitHandler(e: React.MouseEvent<HTMLElement>) {
+    e.preventDefault();
+    const type = router.pathname.split('/')[1];
+    const emailCur = emailInput.current.value;
+    const nameCur = nameInput.current.value;
+    const passwordCur = password.current.value;
+    const rePasswordCur = rePassword.current.value;
+    try {
+      switch (type) {
+        case RouteEnum.LOGIN:
+          console.log(1);
+          break;
+        case RouteEnum.SIGNUP:
+          const { isValid: emailValid, validMessage: emailMessage } = emailValidator(emailCur);
+          const { isValid: nameValid, validMessage: nameMessage } = nameValidator(nameCur);
+          const { isValid: passwordValid, validMessage: passwordMessage } = passwordValidator(
+            passwordCur,
+            rePasswordCur
+          );
+          if (!passwordValid) throw new Error(passwordMessage);
+      }
+    } catch (e) {
+      if (e instanceof Error) {
+        console.log(e.message);
+      }
+    }
+  }
   return (
     <form action="" className={styles.formAuth}>
       <h2 className={styles.title}>{isFull ? 'Sign up to Trello' : 'Log in to Trello'}</h2>
-      <input type="text" placeholder="Enter email" className={styles.input} defaultValue={email} />
-      {isFull ? <input type="text" placeholder="Enter name" className={styles.input} /> : <></>}
-      <input type="password" placeholder="Enter password" className={styles.input} />
+      <input
+        type="email"
+        ref={emailInput}
+        placeholder="Enter email"
+        className={styles.input}
+        defaultValue={email}
+      />
       {isFull ? (
-        <input type="password" placeholder="Re-enter password" className={styles.input} />
+        <input type="text" ref={nameInput} placeholder="Enter name" className={styles.input} />
+      ) : (
+        <></>
+      )}
+      <input type="password" ref={password} placeholder="Enter password" className={styles.input} />
+      {isFull ? (
+        <input
+          type="password"
+          ref={rePassword}
+          placeholder="Re-enter password"
+          className={styles.input}
+        />
       ) : (
         <></>
       )}
       <button
         type="submit"
         className={isFull ? `${styles.submitBtn} ${styles.signup}` : `${styles.submitBtn}`}
+        onClick={(e) => {
+          submitHandler(e);
+        }}
       >
         {isFull ? 'Sign up' : 'Log in'}
       </button>
---
+++ utils/validator.ts
@@ -0,0 +1,28 @@
+interface IValidator {
+  isValid: boolean;
+  validMessage?: string;
+}
+export function passwordValidator(pass: string, rePass: string): IValidator {
+  if (pass.length === 0) return generateValidatorAnswer(false, 'Please, enter password');
+  if (pass.length < 4) return generateValidatorAnswer(false, 'Password is too short');
+  if (!pass.match(/\d/g))
+    return generateValidatorAnswer(false, 'Password must contain at least 1 digit');
+  if (rePass.length === 0) return generateValidatorAnswer(false, 'Please, re-enter password');
+  if (pass !== rePass) return generateValidatorAnswer(false, 'Passwords do not match');
+  return generateValidatorAnswer(true);
+}
+export function emailValidator(email: string): IValidator {
+  const valid =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email.toLowerCase()
+    );
+  return valid ? generateValidatorAnswer(valid) : generateValidatorAnswer(valid, 'Invalid email');
+}
+export function nameValidator(name: string): IValidator {
+  if (name.length === 0) return generateValidatorAnswer(false, 'Please enter name');
+  if (name.length < 4) return generateValidatorAnswer(false, 'Name is too short');
+  return generateValidatorAnswer(true);
+}
+function generateValidatorAnswer(isValid: boolean, message?: string): IValidator {
+  return { isValid, validMessage: message };
+}
