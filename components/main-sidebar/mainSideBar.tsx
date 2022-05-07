ea029e788c6a47b76df5be50104d582024115942
Shamshod <abdushamshod@gmail.com>
2022-05-07 07:55:40 +0500

    feat: setup fontawesome for icons

--- components/main-sidebar/mainSideBar.tsx
+++ components/main-sidebar/mainSideBar.tsx
@@ -1,22 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect } from 'react';
 import Assd from './asdas';
 import { useRouter } from 'next/router';
 import { ThemeProvider } from '@emotion/react';
 import theme from './theme';
+import styles from './Styles.module.scss';
 
 export default function MainSideBar() {
   const router = useRouter();
-  console.log(router);
-
   useEffect(() => {}, []);
 
   return (
     <ThemeProvider theme={theme}>
-      <aside color="secondary" style={{ background: '#3245' }}>
+      <aside color="secondary" className={styles.m_sidebar}>
         <Assd />
         <ul>
           <li>
-            <a color="secondary">Доски</a>
+            <a className={styles.color}>Доски</a>
           </li>
           <li>
             <a>Шаблоны</a>
