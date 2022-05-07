036d2d026efbf9a8fb7d3f9a8a3ad017360ba48c
buterbrot359 <batowka359@gmail.com>
2022-05-07 13:49:22 +0600

    fix: 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,8 @@ import { useRouter } from 'next/router';
 import { WelcomeHeader } from '../components/welcome/WelcomeHeader';
 import { HeroSection } from '../components/welcome/HeroSection';
 import { ProductsSection } from '../components/welcome/ProductsSection';
-
+import styles from '../styles/Welcome.module.scss';
 const Welcome: NextPage = () => {
-  const router = useRouter();
-
   return (
     <>
       <WelcomeHeader />
