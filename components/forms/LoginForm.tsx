12a82c1f449065af4337ad5ade9f7e5a240c75ce
buterbrot359 <batowka359@gmail.com>
2022-05-11 00:35:27 +0600

    add: notification about errors

--- components/forms/LoginForm.tsx
+++ components/forms/LoginForm.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRef, useState } from 'react';
-import { Button, TextField, Typography } from '@mui/material';
+import { Alert, AlertTitle, Button, Collapse, Slide, TextField, Typography } from '@mui/material';
 import styles from '../../styles/form/Form.module.scss';
 import { LoginUser } from '../../api/user';
 import { FormError } from '../../utils/error';
@@ -10,6 +10,8 @@ const LoginForm = () => {
   const passwordInput = useRef() as React.MutableRefObject<HTMLInputElement>;
   const [emptyPswError, setEmptyPswError] = useState(false);
   const [emptyEmailError, setEmptyEmailError] = useState(false);
+  const [errorNotification, setErrorNotification] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   async function submitHandler(e: React.MouseEvent<HTMLElement>) {
     e.preventDefault();
@@ -23,7 +25,15 @@ const LoginForm = () => {
         throw new Error(response.message);
       }
       localStorage.setItem('token', response.token);
-    } catch (error) {}
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+        setErrorNotification(true);
+        setTimeout(() => {
+          setErrorNotification(false);
+        }, 2000);
+      }
+    }
   }
 
   function checkEmptyFields(email: string, password: string) {
@@ -41,50 +51,61 @@ const LoginForm = () => {
   }
 
   return (
-    <form action="" className={styles.formAuth}>
-      <Typography variant="h5" gutterBottom component="h2">
-        Log in to Trello
-      </Typography>
-      <TextField
-        required
-        error={emptyEmailError}
-        id="outlined-required"
-        label="Email"
-        defaultValue=""
-        inputRef={emailInput}
-        helperText={emptyEmailError ? 'Please, enter your email' : ''}
-        fullWidth
-      />
-      <TextField
-        error={emptyPswError}
-        id="outlined-password-input"
-        label="Password"
-        type="password"
-        autoComplete="current-password"
-        helperText={emptyPswError ? 'Please, enter your password' : ''}
-        inputRef={passwordInput}
-        fullWidth
-      />
-      <Button
-        type="submit"
-        variant="contained"
-        color="success"
-        size="large"
-        fullWidth
-        onClick={(e) => {
-          submitHandler(e);
-        }}
-      >
-        Log in
-      </Button>
-      <hr className={styles.separateLine} />
-      <ul className={styles.formLinks}>
-        <li className={styles.formLinkText}>Don't have an account ?</li>
-        <li className={styles.formLinkText}>
-          <Link href={'/signup'}>Sign up</Link>
-        </li>
-      </ul>
-    </form>
+    <>
+      <form action="" className={styles.formAuth}>
+        <Typography variant="h5" gutterBottom component="h2">
+          Log in to Trello
+        </Typography>
+        <TextField
+          required
+          error={emptyEmailError}
+          id="outlined-required"
+          label="Email"
+          defaultValue=""
+          inputRef={emailInput}
+          helperText={emptyEmailError ? 'Please, enter your email' : ''}
+          fullWidth
+        />
+        <TextField
+          error={emptyPswError}
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          helperText={emptyPswError ? 'Please, enter your password' : ''}
+          inputRef={passwordInput}
+          fullWidth
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="success"
+          size="large"
+          fullWidth
+          onClick={(e) => {
+            submitHandler(e);
+          }}
+        >
+          Log in
+        </Button>
+        <hr className={styles.separateLine} />
+        <ul className={styles.formLinks}>
+          <li className={styles.formLinkText}>Don't have an account ?</li>
+          <li className={styles.formLinkText}>
+            <Link href={'/signup'}>Sign up</Link>
+          </li>
+        </ul>
+        <Collapse in={errorNotification}>
+          <Slide in={errorNotification} direction="left">
+            <Alert severity="error" style={{ position: 'fixed', top: 20, right: 20 }}>
+              <AlertTitle>
+                <strong>Error:</strong> {errorMessage}
+              </AlertTitle>
+            </Alert>
+          </Slide>
+        </Collapse>
+      </form>
+    </>
   );
 };
 
