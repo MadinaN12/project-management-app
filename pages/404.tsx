05243d4bbeeab73cdd78109a67ea3e7c5996b608
Shamshod <abdushamshod@gmail.com>
2022-05-10 19:55:48 +0500

    feat: add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,38 @@
+import { Container, Typography } from '@mui/material';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+export default function ErrorPage() {
+  const [seconds, setSeconds] = useState(3);
+  const titleStyle = {
+    mt: 5,
+    padding: 5,
+    textAlign: 'center',
+    fontWeight: 600,
+  };
+  const linkStyle = { color: '#1000ff', display: 'inline', cursor: 'pointer' };
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => router.push('/'), 3000);
+    if (seconds) setInterval(() => setSeconds((e) => e - 0.5), 1000);
+  }, []);
+
+  return (
+    <>
+      <Container sx={titleStyle}>
+        <Typography variant="h3">Oops, this page not found</Typography>
+
+        <Typography variant="subtitle1" sx={titleStyle}>
+          Redirecting to{' '}
+          <Link href={'/'} passHref>
+            <a style={linkStyle}> main page </a>
+          </Link>
+          in {seconds} seconds...
+        </Typography>
+      </Container>
+    </>
+  );
+}
