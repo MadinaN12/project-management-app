53867ca65de0d379ee6faf0322f1780af5393aec
Shamshod <abdushamshod@gmail.com>
2022-05-13 11:22:55 +0500

    init: header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { setUpStore } from '../store/store';
 import { Provider } from 'react-redux';
+import Header from '../components/header';
 
 const store = setUpStore();
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
+      <Header />
       <Component {...pageProps} />
     </Provider>
   );
