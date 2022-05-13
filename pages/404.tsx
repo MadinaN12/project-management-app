757484674b608eb68e60f3cb5e10a13253b55ca9
Shamshod <abdushamshod@gmail.com>
2022-05-13 21:18:33 +0500

    refactor: changes style sx of 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,23 +2,22 @@ import { Container, Typography } from '@mui/material';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { titleStyle, linkStyle } from '../styles/404/404_style';
 
 export default function ErrorPage() {
   const [seconds, setSeconds] = useState(3);
-  const titleStyle = {
-    mt: 5,
-    padding: 5,
-    textAlign: 'center',
-    fontWeight: 600,
-  };
-  const linkStyle = { color: '#1000ff', display: 'inline', cursor: 'pointer' };
+
+  const [countStatus, setCountStatus] = useState(false);
 
   const router = useRouter();
 
   useEffect(() => {
-    setTimeout(() => router.push('/'), 3000);
-    if (seconds) setInterval(() => setSeconds((e) => e - 0.5), 1000);
-  }, [router, seconds]);
+    if (!countStatus) {
+      setTimeout(() => router.push('/'), 3000);
+      if (seconds) setInterval(() => setSeconds((e) => e - 0.5), 1000);
+    }
+    setCountStatus(true);
+  }, [countStatus, router, seconds]);
 
   return (
     <>
