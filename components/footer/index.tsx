71af409cc8aadf9413fa102c4516096a5ea762f9
Shamshod <abdushamshod@gmail.com>
2022-05-17 21:40:52 +0500

    refactor: changes from review

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,30 +3,24 @@ import { Box } from '@mui/system';
 import Image from 'next/image';
 import { footerStyle } from '../../styles/footer';
 import Logo from '../../images/rs_school_js.svg';
+import { dataUsers } from './dataUsers';
 
 export default function Footer() {
-  const dataUsers = [
-    { link: 'https://github.com/MadinaN12', id: '1', name: 'MadinaN12' },
-    { link: 'https://github.com/ButerBrot359', id: '2', name: 'ButterBrot359' },
-    { link: 'https://github.com/ShamshodIsayev', id: '3', name: 'ShamshodIsayev' },
-  ];
   return (
-    <>
-      <footer style={footerStyle.footer}>
-        <Box sx={footerStyle.words}>
-          {dataUsers.map((user) => (
-            <Link underline="hover" sx={footerStyle.link} href={user.link} key={user.id}>
-              {user.name}
-            </Link>
-          ))}
-        </Box>
-        <Box sx={footerStyle.box}>
-          <Link href="https://rs.school/react/" sx={{ width: '100px' }}>
-            <Image style={footerStyle.img} src={Logo} alt="rs" />
+    <footer style={footerStyle.footer}>
+      <Box sx={footerStyle.words}>
+        {dataUsers.map((user) => (
+          <Link underline="hover" sx={footerStyle.link} href={user.link} key={user.id}>
+            {user.name}
           </Link>
-          <Typography sx={{ m: '14px' }}>2022.05</Typography>
-        </Box>
-      </footer>
-    </>
+        ))}
+      </Box>
+      <Box sx={footerStyle.box}>
+        <Link href="https://rs.school/react/" sx={{ width: '100px' }}>
+          <Image style={footerStyle.img} src={Logo} alt="rs" />
+        </Link>
+        <Typography sx={{ m: '14px' }}>2022.05</Typography>
+      </Box>
+    </footer>
   );
 }
