b92392f807b143d9d988ae86e3c2b3ba2479e2d9
Shamshod <abdushamshod@gmail.com>
2022-05-30 06:01:07 +0500

    feat: add basic changes to UI

--- components/header/HeaderBtns.tsx
+++ components/header/HeaderBtns.tsx
@@ -4,7 +4,7 @@ import router from 'next/router';
 export default function HeaderBtns({ handleSignOut }: { handleSignOut: VoidFunction }) {
   return (
     <>
-      <Button variant="text" size="small" onClick={() => router.push('/')}>
+      <Button variant="text" size="small" sx={{ mr: 2 }} onClick={() => router.push('/profile')}>
         Edit Profile
       </Button>
       <Button variant="contained" size="small" onClick={handleSignOut}>
